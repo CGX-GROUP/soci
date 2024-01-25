208e4cceca459dffe32954dff7dbdcb5e6e1523c
DELPAS Cédric <cedric.delpas@cgx-group.com>
2024-01-25 18:24:56 +0100

    Suppression de l'operator= sur les blob

--- include/soci/blob.h
+++ include/soci/blob.h
@@ -28,6 +28,7 @@ class SOCI_DECL blob
 public:
     explicit blob(session & s);
     blob(const blob&) = delete;
+    void operator=(const blob&) = delete;
     ~blob();
 
     std::size_t get_len() const;
